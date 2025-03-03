af7e4c9cc2bb7077ef518cb27477b4b462f3c24b
ankit007 <ankitkum837@gmail.com>
2025-03-03 23:34:25 +0530

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <h3 className={styles.aboutdes}>Hello! I'm a passionate web developer with a strong foundation in JavaScript, React.js, Node.js, Tailwind CSS, HTML, and CSS. 
+        I thrive on creating dynamic and responsive web applications that provide seamless user experiences.
+        With a keen eye for design and a deep understanding of front-end and back-end technologies,
+         I enjoy building solutions that are both visually appealing and highly functional.</h3>
+      <div className={styles.content}>
+        <img
+          src="https://img.freepik.com/premium-vector/hand-typing-laptop-writer-workplace-social-network-vector-flat-cartoon-illustration_501804-60.jpg?w=2000" 
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:ankitkum837@gmail.com">Ankit kumar</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/ankitpadit/">linkedin.com/Ankit</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/ankitkum007?tab=repositories">github.com/ankitkum007</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
